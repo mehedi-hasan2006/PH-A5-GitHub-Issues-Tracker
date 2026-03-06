c5d4750c259fe789a99ccd44f889f9d2f16ce05e
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-03-06 20:28:55 +0600

    login page & login function implement done & home page's navbar done

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const login = () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    window.location.assign("/home.html");
+  } else {
+    alert("login failed");
+    return;
+  }
+};
+
